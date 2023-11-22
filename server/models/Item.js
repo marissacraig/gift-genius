6d9eb742f9a9def970b1f9051f77e885f7d30dd4
desktopbuddy <hanchondo21@gmail.com>
2023-11-22 01:23:44 -0800

    create Item model

---
+++ server/models/Item.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Item must have a title',
+    minlength: 1,
+    maxlength: 60,
+    trim: true,
+  },
+  url: {
+    type: String
+  },
+  image: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  wishability: {
+    type: Number
+  },
+  fulfilled: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const Item = model('Item', itemSchema);
+
+module.exports = Item;
