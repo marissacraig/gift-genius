500fa5cd831cca47037153d3689ecbcdcf788e9a
marissacraig <gersen.marissa@gmail.com>
2023-11-26 10:55:58 -0800

    created GiftContext.jsx in utils

---
+++ client/src/utils/GiftContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useReducer } from 'react';
+
+import reducer from './reducers';
+
+// Initialize new context for students
+const GiftContext = createContext();
+
+// We create a custom hook to immediately pass the student context value ({ students, majors } - see line 36) to any consumer component which invokes it
+export const useGiftContext = () => useContext(GiftContext);
+
+// The provider is responsible for creating our state, updating the state, and persisting values to the children
+export const GiftProvider = ({ children }) => {
+  const gifts = [
+    {
+      id: 1,
+      name: 'Legos',
+      price: '$20',
+    },
+    {
+      id: 2,
+      name: 'Guitar',
+      price: '$250',
+    },
+  ];
+
+  const appState = useReducer(reducer, { gifts, prices })
+
+  // The value prop expects an initial state object
+  return (
+    <GiftContext.Provider value={appState}>
+      {/* We render children in our component so that any descendent can access the value from the provider */}
+      {children}
+    </GiftContext.Provider>
+  );
+};
