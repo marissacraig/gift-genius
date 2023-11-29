291fd89501f1a130ba063112f3b8c0a300a70199
desktopbuddy <hanchondo21@gmail.com>
2023-11-29 14:09:04 -0800

    create ItemCard component

--- client/src/components/ItemCard/index.jsx
+++ client/src/components/ItemCard/index.jsx
@@ -1,14 +1,13 @@
+import "./index.css"
 const ItemCard = ( {item} ) => {
     return (
       <div key={item._id} className="item-card">
-        <h4>{item.name}</h4>
+        <div className="card-header my-2 flex-row space-between">
+          <h4>{item.name}</h4>
+          {/* edit button */}
+          <button>Edit</button>
+        </div>
 
-        {/* display whether or not item has been fulfilled */}
-        {item.fulfilled ? (
-          <p>Fulfilled</p>
-        ) : (
-          <p>Not fulfilled</p>
-        )}
         
         {/* display price is price exists */}
         {item.price && (
@@ -21,7 +20,17 @@ const ItemCard = ( {item} ) => {
           <a href={item.url}>↗ URL</a>
         )}
 
-        
+        <div className="fulfilled my-2 d-flex flex-row justify-content-end">
+          {/* display whether or not item has been fulfilled */}
+          {item.fulfilled ? (
+            <p>Fulfilled</p>
+          ) : (
+            <p>Not fulfilled</p>
+          )}
+
+          {/* Click to mark as fulfilled */}
+          <h3 class="fulfill-button">✔</h3>
+        </div>
       </div>
     );
   };
